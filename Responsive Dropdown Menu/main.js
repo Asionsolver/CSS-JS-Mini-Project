3d02eaf0bc7e141c58d53ed7d392ac2151492ee7
Asionsolver <asionsolver274@gmail.com>
2023-05-24 13:31:54 +0600

    complete Responsive Dropdown Menu with Sub Menu

---
+++ Responsive Dropdown Menu/main.js
@@ -0,0 +1,6 @@
+let menuToggle = document.querySelector('.menu-toggle');
+let header = document.querySelector('header');
+
+menuToggle.onclick =  function () {
+    header.classList.toggle('active');
+}
